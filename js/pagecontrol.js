1273740963bda2ad2e22d67e9705b0fad00c9100
Azhar Kalarickal <48583694+Azharkalarickal@users.noreply.github.com>
2023-06-20 17:32:14 +0530

    Add files via upload

---
+++ js/pagecontrol.js
@@ -0,0 +1,98 @@
+
+    document.getElementById("abb").style.display = "block";
+    document.getElementById("dos").style.display = "block";
+    document.getElementById("pot").style.display = "block";
+    document.getElementById("con").style.display = "block";
+    document.getElementById("ski").style.display = "none";
+    document.getElementById("cli").style.display = "none";
+    document.getElementById("loginp").style.display = "none";
+
+        document.getElementById("pro1").style.display = "none";
+        document.getElementById("pro2").style.display = "none";
+        document.getElementById("pro3").style.display = "none";
+        document.getElementById("pro4").style.display = "none";
+    function myFunctionhm() {
+      
+        document.getElementById("abb").style.display = "block";
+        document.getElementById("dos").style.display = "block";
+        document.getElementById("pot").style.display = "block";
+        document.getElementById("con").style.display = "block";
+        document.getElementById("cli").style.display = "none";
+        document.getElementById("ski").style.display = "none";
+        document.getElementById("hmpage").style.display = "block";
+        document.getElementById("loginp").style.display = "none";
+        document.getElementById("sel").style.display = "block";
+      }
+    function myFunctionab() {
+      
+      document.getElementById("abb").style.display = "block";
+      document.getElementById("dos").style.display = "none";
+      document.getElementById("pot").style.display = "none";
+      document.getElementById("con").style.display = "none";
+      document.getElementById("cli").style.display = "none";
+      document.getElementById("ski").style.display = "none";
+      document.getElementById("hmpage").style.display = "block";
+      document.getElementById("loginp").style.display = "none";
+      document.getElementById("sel").style.display = "none";
+    }
+    function myFunctionpot() {
+      
+        document.getElementById("abb").style.display = "none";
+        document.getElementById("dos").style.display = "none";
+        document.getElementById("pot").style.display = "block";
+        document.getElementById("con").style.display = "none";
+        document.getElementById("cli").style.display = "none";
+        document.getElementById("ski").style.display = "none";
+        document.getElementById("hmpage").style.display = "block";
+        document.getElementById("loginp").style.display = "none";
+        document.getElementById("sel").style.display = "none";
+      }
+      function myFunctionser() {
+      
+        document.getElementById("abb").style.display = "none";
+        document.getElementById("dos").style.display = "block";
+        document.getElementById("pot").style.display = "none";
+        document.getElementById("con").style.display = "none";
+        document.getElementById("cli").style.display = "none";
+        document.getElementById("ski").style.display = "none";
+        document.getElementById("hmpage").style.display = "block";
+        document.getElementById("loginp").style.display = "none";
+        document.getElementById("sel").style.display = "none";
+      }
+      function myFunctioncon() {
+      
+        document.getElementById("abb").style.display = "none";
+        document.getElementById("dos").style.display = "none";
+        document.getElementById("pot").style.display = "none";
+        document.getElementById("con").style.display = "block";
+        document.getElementById("cli").style.display = "none";
+        document.getElementById("ski").style.display = "none";
+        document.getElementById("hmpage").style.display = "block";
+        document.getElementById("loginp").style.display = "none";
+        document.getElementById("sel").style.display = "none";
+      }
+      function myFunctionlog() {
+      
+        document.getElementById("abb").style.display = "none";
+        document.getElementById("dos").style.display = "none";
+        document.getElementById("pot").style.display = "none";
+        document.getElementById("con").style.display = "none";
+        document.getElementById("cli").style.display = "none";
+        document.getElementById("ski").style.display = "none";
+        document.getElementById("hmpage").style.display = "none";
+        document.getElementById("loginp").style.display = "block";
+      }
+      function myFunctionlogedin() {
+      
+        document.getElementById("form").style.display = "none";
+        document.getElementById("log1").style.display = "none";
+        document.getElementById("log2").style.display = "none";
+        document.getElementById("log3").style.display = "none";
+        document.getElementById("log4").style.display = "none";
+        document.getElementById("pro1").style.display = "block";
+        document.getElementById("pro2").style.display = "block";
+        document.getElementById("pro3").style.display = "block";
+        document.getElementById("pro4").style.display = "block";
+
+      }
+   
